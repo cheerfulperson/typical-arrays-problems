ca8dfd6addb36fd7e7fd8946004e4a2977af9bf5
cheerfulperson <43146047+cheerfulperson@users.noreply.github.com>
2020-09-22 18:56:36 +0300

    init: it is done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if( array == null || array[0] == undefined ){
+    return 0;
+  }
+  let arr = array.sort((a, b) => a - b);
+  return arr[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if( array == null || array[0] == undefined ){
+    return 0;
+  }
+  let arr = array.sort((a, b) => a - b).reverse();
+  return arr[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if( array == null || array[0] == undefined ){
+    return 0;
+  }
+  let num = 0;
+  for(n in array){
+    num += array[n];
+  }
+  num = num / array.length
+  return num;
 }
